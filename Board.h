b85dbd2e252ee01b3a8e02749556910d5e9d4ef7
elizabethhom <59620870+elizabethhom@users.noreply.github.com>
2020-08-11 22:29:33 -0700

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,12 +1,12 @@
-/********************************************
-* Comp 15 - Fall 2019
-* Project 1
-* ELIZABETH HOM
-* 23 OCT 2019
-* 
-* Board interface
-* Class interface for the Board class
-*********************************************/
+/*
+ * Board.h
+ * 
+ * COMP15 - FALL2019
+ * Elizabeth Hom (ehom01)
+ * Last Modified: October 23, 2019
+ * 
+ * Purpose: Interface for Board class.
+ */
 
 #include <iostream>
 
@@ -15,6 +15,11 @@ using namespace std;
 #ifndef BOARD_H_
 #define BOARD_H_
 
+/*
+ * Robot
+ *
+ * Describes the robot by its symbol and row/col indices.
+ */
 struct Robot {
     char symbol;
     int row, col, indexRow, indexCol;
@@ -23,44 +28,42 @@ struct Robot {
 class Board
 {
     public:
-        // default constructor
+    
         Board();
-
-        // destructor
         ~Board();
 
-        // places robot symbol in the specified row and col
+        // Places robot symbol at row/col
         void placeRobot(char symbol, int row, int col);
 
-        // gets robot's current row, col index
+        // Gets robot's current row/col
         int getRobotRowInd();
         int getRobotColInd();
 
-        // gets board character at specified row, col index
+        // Gets board character at row/col
         char getBoardChar(int rowInd, int colInd);
 
-        // reads in board and saves into board array
+        // Reads and populates board
         void fillBoard(string filename);
 
-        // prints current state of board
+        // Prints board 
         void printBoard();
 
-        // moves robot as far as possible in specified direction
+        // Moves robot as far as possible in respective direction
         void moveEast();
         void moveWest();
         void moveSouth();
         void moveNorth();
 
-        // returns true if conditions to win are met
+        // Returns true if win conditions met
         bool getYouWin();
 
-        // returns true if robot can move in specified direction
+        // Returns true if robot can move in respective direction
         bool canMoveNorth(int rowIndex, int colIndex);
         bool canMoveEast(int rowIndex, int colIndex);
         bool canMoveSouth(int rowIndex, int colIndex);
         bool canMoveWest(int rowIndex, int colIndex);
 
-        // returns row or col of robot if it moved in specified direction
+        // Returns robot row/col if can move in respective direction
         int mvEGetPos(int rowIndex, int colIndex);
         int mvSGetPos(int rowIndex, int colIndex);
         int mvWGetPos(int rowIndex, int colIndex);
